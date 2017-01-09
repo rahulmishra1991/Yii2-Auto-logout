1e8d7f27f7412259ca6618d84a3c13b3e30d77b9
rahulmishra1991 <rahulmishra1991@users.noreply.github.com>
2017-01-09 18:24:27 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
+        'mycollapse' => [
+            'class' => 'app\components\MyCollapse',
+        ],
+        'user' => array(
+             //enable cookie-based authentication
+            'enableAutoLogin' => false,
+			'authTimeout' => 120,
+        ),
+    ],
+];
